8585371163df6c13f733c7503998b95b092ef25d
Grigory Vasiliev <Grigory.Vasiliev@software.dell.com>
2014-07-15 21:47:30 +0000

    snapshot id from ami

--- orphaned_snapshots.py
+++ orphaned_snapshots.py
@@ -10,7 +10,27 @@ ec2Connection = boto.connect_ec2(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 imgs = ec2Connection.get_all_images(owners=['self'])
 
 print len(imgs)
-print str(imgs[0].description) + imgs[0].id
+print str(imgs[0].architecture)
+print str(imgs[0].billing_products)
+for i in imgs[0].block_device_mapping.items():
+    print "  " + str(i[1].snapshot_id)
+print str(imgs[0].description)
+print str(imgs[0].id)
+print str(imgs[0].instance_lifecycle)
+print str(imgs[0].item)
+print str(imgs[0].kernel_id)
+print str(imgs[0].location)
+print str(imgs[0].name)
+print str(imgs[0].ownerId)
+print str(imgs[0].owner_alias)
+print str(imgs[0].platform)
+print str(imgs[0].product_codes)
+print str(imgs[0].root_device_name)
+print str(imgs[0].root_device_type)
+print str(imgs[0].state)
+print str(imgs[0].tags)
+print str(imgs[0].type)
+print str(imgs[0].virtualization_type)
 
 
 # Get the list of registered AMIs.
@@ -43,15 +63,15 @@ for snapshot in ec2Connection.get_all_snapshots(owner='self'):
         else:
             imageSnapshots[snapshotImageId] = [snapshotId]
 
-print "Mapped snapshots:"
-for image in sorted(imageSnapshots.keys()):
-    print image + ": " + imageNames[image]
-    for snapshot in imageSnapshots[image]:
-        print "\t- " + snapshot
+# print "Mapped snapshots:"
+# for image in sorted(imageSnapshots.keys()):
+    # print image + ": " + imageNames[image]
+    # for snapshot in imageSnapshots[image]:
+        # print "\t- " + snapshot
 
-print
-print "Orphans: " + ','.join(sorted(snapshotsToDelete))
-print
-print "Unknown: " + ','.join(sorted(snapshotsUnknown))
+# print
+# print "Orphans: " + ','.join(sorted(snapshotsToDelete))
+# print
+# print "Unknown: " + ','.join(sorted(snapshotsUnknown))
 # Close connection to EC2.
 ec2Connection.close()
