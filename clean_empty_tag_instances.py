d114566790284816c60675dadc11f8a61b72387a
Grigory Vasiliev <Grigory.Vasiliev@software.dell.com>
2014-11-10 19:58:23 +0000

    added clean instances with no team tag pilicy

--- clean_empty_tag_instances.py
+++ clean_empty_tag_instances.py
@@ -8,12 +8,10 @@ import boto
 all = [r.instances[0] for r in c.get_all_instances( filters = {"instance-lifecycle":"spot","instance-state-name":"running"})]
 import datetime
 now = datetime.datetime.now()
-#print "Total count: %i for %s" % ( len(all), now )
+print "Total count: %i for %s" % ( len(all), now )
 
 from dateutil import parser
 from datetime import timedelta
-
-# print 'All instances'
 
 def loginst( i ):
     t = parser.parse(i.launch_time).replace(tzinfo=None)
@@ -23,14 +21,12 @@ def loginst( i ):
     if i.tags.has_key('protection'): p = i.tags['protection']
     print "%.1fh,%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % ( (now - t).total_seconds() / 3600,(now - t).total_seconds(), n, team, i.id, i.state, i.instance_type, i.spot_instance_request_id, i.platform, p, i.root_device_name, i.root_device_type )
 
-# for i in all:
-    # loginst( i )
-        
-print 'Instances for termination at night hours'
-        
+print 'Instances for termination with no team tag'
+print "Lifetime(h),Lifetime(sec),Name,team,id,state,instance_type,spot_request_id,platform,protection"
 for i in all:
-    if not i.tags.has_key('protection') and i.state == 'running' and ( i.instance_type not in ['t1.micro','m1.small'] or i.platform == 'windows' ):
+    t = (now - parser.parse(i.launch_time).replace(tzinfo=None)).total_seconds()
+    if ( not i.tags.has_key('team') or i.tags['team'] not in ['dmp','mmex','rmad','rmex', 'sasp', 'ecolabs', 'rmsp'] ) and t > 1800:
         loginst( i )
-        i.terminate() 
+        i.terminate()
 
 print "work has completed"
